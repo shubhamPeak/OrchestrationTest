c5de92a49d0ac8d0cf3853c96af9e3ce3948ba4f
Punit Tiwari <39051765+punittiwari01@users.noreply.github.com>
2018-10-17 19:22:46 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -8,7 +8,7 @@ import io
 if __name__ == "__main__":
     url = "postgresql://{user}:{password}@{host}:{port}/{dbname}".format(
                   #dbname="punit_test_001",
-                  dbname="elchipo",
+                  dbname="testclient01",
                   user=os.getenv("REDSHIFT_USERNAME"),
                   password=os.getenv("REDSHIFT_PASSWORD"),
                   host = os.getenv("REDSHIFT_HOST"),
@@ -16,5 +16,5 @@ if __name__ == "__main__":
                   PEAK_API_KEY = os.getenv('PEAK_API_KEY')
                  )
     engine = create_engine(url)
-    #engine.execute("INSERT INTO publish.q1 (firstname, email) VALUES ('name11','mail111@mail.com');")
-    engine.execute("INSERT INTO publish.testqa (id ,name, email) VALUES (1,'Python1','Python1@mail.com');")
+    engine.execute("INSERT INTO publish.q1 (firstname, email) VALUES ('name21','mail21@mail.com');")
+    #engine.execute("INSERT INTO publish.testqa (id ,name, email) VALUES (3,'Python3','Python3@mail.com');")
