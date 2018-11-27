0ae91aa27bea97c3dfc8c225841c8bd3e0b4fbf8
Punit Tiwari <39051765+punittiwari01@users.noreply.github.com>
2018-11-27 11:57:12 +0530

    .
    .

---
+++ PackageInstallCheck.r
@@ -0,0 +1,68 @@
+## Script to test package installation
+# Requires a connection to Redshift. 
+
+## Connection type (ODBC, POSTGRESQL)
+CONNECTION = 'ODBC'
+
+## functions 
+check_package_installation <- function(test_packages = NULL, conn = NULL) {
+  
+  test_packages$installed <- unlist(lapply(test_packages$package, FUN = function(x) {
+    as.character(require(x, character.only = TRUE))
+  }))
+  
+  test_packages$test_date <- as.character(Sys.Date())
+
+  if(!is.null(conn)) {
+   ace <-  DBI::dbWriteTable(conn = conn,
+                       name = "package_check",
+                       value = test_packages,
+                       append = TRUE
+                      )
+  }
+  
+  return(test_packages)
+}
+
+
+
+### set up db connection
+
+if (CONNECTION == 'ODBC') {
+conn <- DBI::dbConnect(odbc::odbc(), "common")
+} else if (CONNECTION == 'POSTGRESQL'){
+  ### NEED to ADD alternative connection type here
+    conn <- dbConnect(dbDriver("PostgreSQL"),
+                 dbname = "shubhamsharma",
+                 host = HOST,
+                 port = 5439,
+                 user = USERNAME,
+                 password = PASSWORD
+)
+}
+
+
+## specifiy packages to test here 
+packages_to_test <- c('dplyr', 'fake_package') 
+
+
+test_packages <- data.frame(package = packages_to_test,
+                            installed = NA,
+                            test_date = NA)
+
+
+## RUN check
+test_packages <- check_package_installation(test_packages, conn = conn)
+
+
+
+
+
+
+
+
+
+
+
+
+
