8698cf9c38af0f6bcab36f37dc57ebf8397b7fb3
Shubham <shubham.prasad@peak.ai>
2018-12-01 16:19:04 +0530

    Update test2.r

--- test2.r
+++ test2.r
@@ -8,11 +8,11 @@ print(USERNAME)
 print(PASSWORD)
 print(HOST)
 
-con <- dbConnect(dbDriver("PostgreSQL"),
-                 dbname = "elchipo",
+con <- dbConnect(dbDriver("RPostgreSQL"),
+                 dbname = "shubhamjupy",
                  host = HOST,
                  port = 5439,
                  user = USERNAME,
                  password = PASSWORD
 )
-dbGetQuery(con,"INSERT INTO publish.testqa (id, name, email) VALUES (1,'R1','R1@mail.com');")
+dbGetQuery(con,"INSERT INTO publish.shubham2 (id, name) VALUES (1,'R1');")
